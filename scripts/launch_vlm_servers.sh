694c8c03059d60210ed9dce8fcf99d6ef09bfb59
naokiyokoyama <naoki.yokoyama7@gmail.com>
2025-03-03 15:18:05 -0800

    using while loop for vlms in case they break

--- scripts/launch_vlm_servers.sh
+++ scripts/launch_vlm_servers.sh
@@ -37,11 +37,11 @@ done
 $tm split-window -v -t ${session_name}:0
 
 # Run commands in each pane
-$tm send-keys -t ${session_name}:0.0 "source ${env_vars} && cd ${VLFM_DIR} && ${VLFM_PYTHON} -m vlfm.vlm.sam --port ${SAM_PORT}" C-m
-$tm send-keys -t ${session_name}:0.1 "source ${env_vars} && cd ${VLFM_DIR} && ${VLFM_PYTHON} -m vlfm.vlm.yolo --port ${YOLO_PORT}" C-m
+$tm send-keys -t ${session_name}:0.0 "source ${env_vars} && cd ${VLFM_DIR} && while true; do ${VLFM_PYTHON} -m vlfm.vlm.sam --port ${SAM_PORT}; done" C-m
+$tm send-keys -t ${session_name}:0.1 "source ${env_vars} && cd ${VLFM_DIR} && while true; do ${VLFM_PYTHON} -m vlfm.vlm.yolo --port ${YOLO_PORT}; done" C-m
 if $has_o; then
     $tm split-window -v -t ${session_name}:0.1
-    $tm send-keys -t ${session_name}:0.2 "cd ${VLFM_DIR} && ${VLFM_PYTHON} -m vlfm.vlm.owlv2 --port ${OWLV2_PORT}" C-m
+    $tm send-keys -t ${session_name}:0.2 "source ${env_vars} && cd ${VLFM_DIR} && while true; do ${VLFM_PYTHON} -m vlfm.vlm.owlv2 --port ${OWLV2_PORT}; done" C-m
 fi
 
 # Resize the panes so they are all equal size
