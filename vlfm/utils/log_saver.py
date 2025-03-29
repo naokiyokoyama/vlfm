e19cc9a828f5957f92a48e1b73f0753bd6173912
naokiyokoyama <naoki.yokoyama7@gmail.com>
2025-03-29 10:00:07 -0700

    linting

--- vlfm/utils/log_saver.py
+++ vlfm/utils/log_saver.py
@@ -26,17 +26,19 @@ def log_episode(episode_id: Union[str, int], scene_id: str, data: Dict) -> None:
                 {"episode_id": episode_id, "scene_id": scene_id, **data}, f, indent=4
             )
 
+
 def is_json_completed(json_file: str) -> bool:
     try:
         if os.path.getsize(json_file) == 0:
             return False
-        with open(json_file, 'r') as f:
+        with open(json_file, "r") as f:
             content = f.read().strip()
             if content == "" or content == "{}" or "spl" not in content:
                 return False
     except:
         return False
     return True
+
 
 def is_evaluated(
     episode_id: Union[str, int],
